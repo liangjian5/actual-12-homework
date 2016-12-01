fa46e5f3da2156e271ab279ed098337382547841
mxjstone <169010000@qq.com>
2016-12-01 14:37:49 +0800

    Update list_max.py
    update

--- 01/chenshilei/list_max.py
+++ 01/chenshilei/list_max.py
@@ -1,19 +1,19 @@
 #!/usr/bin/evn python
-#search the first max  and the second max
+#_*_ coding:utf-8 _*_
 
+#search the first max  and the second max
 _list=[1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]
 
 #print _list
-max_1=_list[0]
-max_2=0
+max_a=_list[0]
+max_b=0
 temp=0
 for n in _list:
-    if n > max_1:
-        temp=max_1
-        max_1=n
-        max_2=temp
-    elif n != max_1 and n> max_2:
-        max_2=n
+    if n > max_a:
+        temp=max_a
+        max_a=n
+        max_b=temp
+    elif n != max_a and n> max_b:
+        max_b=n
 
-print 'The first max: %s \nThe second max: %s' % (max_1,max_2)
-
+print 'The first max: %s \nThe second max: %s' % (max_a,max_b)
