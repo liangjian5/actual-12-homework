5bf9467747f4fe3357a4fd612dcfebfa0b0a72c9
samuhaha <taoleisure@gmail.com>
2016-12-02 23:09:02 +0800

    Update homework_day_1.py

--- 01/sam/homework_day_1.py
+++ 01/sam/homework_day_1.py
@@ -5,7 +5,7 @@ for num in [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,4
 	if frist_big_num < num:
 		frist_big_num = num
 
-for num in [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]:
-	if second_big_num < num and second_big_num < frist_big_num:
-		second_big_num = num
+for i in [1,2,3,2,12,3,1,3,21,2,2,3,4111,22,3333,444,111,4,5,777,65555,45,33,45]:
+	if second_big_num < i and second_big_num != frist_big_num:
+		second_big_num = i
 print frist_big_num,second_big_num
